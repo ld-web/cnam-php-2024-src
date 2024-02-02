797620adc12b880711a21c9aaab509d10d4cefb6
Lucas Delobelle <lucas@ld-web.net>
2024-02-02 10:16:52 +0100

    :sparkles: PHP Store intro

---
+++ php-store/index.php
@@ -0,0 +1,5 @@
+<?php require_once __DIR__ . '/layout/header.php'; ?>
+
+<h1>Bienvenue</h1>
+
+<?php require_once __DIR__ . '/layout/footer.php'; ?>
---
+++ php-store/layout/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenue !</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">MENU ITEM</a></li>
+                <li><a href="#">MENU ITEM</a></li>
+                <li><a href="#">MENU ITEM</a></li>
+                <li><a href="#">MENU ITEM</a></li>
+                <li><a href="#">MENU ITEM</a></li>
+            </ul>
+        </nav>
+    </header>
